297d438e953ce50b449813e1bc57fa58d042713b
nordicebear <nordicebeargames@gmail.com>
2019-04-01 21:36:02 +0300

    Tetris

---
+++ Tetris/Assets/Scripts/Spawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] spicyObjects;
+
+	void Start ()
+    {
+        spawnRandom();
+	}
+
+    public void spawnRandom()
+    {
+        int index = Random.Range(0, spicyObjects.Length);
+        Instantiate(spicyObjects[index], transform.position, Quaternion.identity);
+    }
+}
